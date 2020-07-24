4bce7004a3ecdc33560d3a793099d20590e15e40
IreneRukumbuzi <rukkirene21@gmail.com>
2020-07-24 10:00:53 +0200

    Created Blog Pages where the users can view the blogs, like/unlike and make a comment. And the Admin can CRUD the blogs

---
+++ UI/Blog-Page/blog.js
@@ -0,0 +1,13 @@
+const togglebtn = document.getElementsByClassName('togglebtn')[0]
+
+const mainNavLinks = document.getElementsByClassName('main-nav')[0]
+
+togglebtn.addEventListener('click', () => {
+    mainNavLinks.classList.toggle('active') 
+})
+
+
+
+
+
+ 
