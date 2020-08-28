75a4911d2cc1ba71a28eb919135d69dfe3d05d14
IreneRukumbuzi <rukkirene21@gmail.com>
2020-08-28 15:27:49 +0200

    Added Axios

--- UI/Contact-page/contact.js
+++ UI/Contact-page/contact.js
@@ -1,63 +1,55 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyAlyd3OzqGEYbvGMYiXRFDYdYQOdPO6Q6Q",
-    authDomain: "my-brand-irene-c2a80.firebaseapp.com",
-    databaseURL: "https://my-brand-irene-c2a80.firebaseio.com",
-    projectId: "my-brand-irene-c2a80",
-    storageBucket: "my-brand-irene-c2a80.appspot.com",
-    messagingSenderId: "981307922858",
-    appId: "1:981307922858:web:c11945d385ba197a3f73d3",
-    measurementId: "G-JPKXJPQ9Q6"
-  };
+const firebaseConfig = {
+  apiKey: 'AIzaSyAlyd3OzqGEYbvGMYiXRFDYdYQOdPO6Q6Q',
+  authDomain: 'my-brand-irene-c2a80.firebaseapp.com',
+  databaseURL: 'https://my-brand-irene-c2a80.firebaseio.com',
+  projectId: 'my-brand-irene-c2a80',
+  storageBucket: 'my-brand-irene-c2a80.appspot.com',
+  messagingSenderId: '981307922858',
+  appId: '1:981307922858:web:c11945d385ba197a3f73d3',
+  measurementId: 'G-JPKXJPQ9Q6',
+};
 firebase.initializeApp(firebaseConfig);
 
-var firestore = firebase.firestore();
+const firestore = firebase.firestore();
 firebase.analytics();
+function getInputVal(id) {
+  return document.getElementById(id).value;
+}
 
-const db = firestore.collection("contactData");
+const db = firestore.collection('contactData');
 
 const form = document.getElementById('myForm').addEventListener('submit', submitForm);
 const errorMessage = document.getElementById('errorMessage');
 let userPosition;
-console.log(userPosition)
+console.log(userPosition);
 
 const success = (position) => {
-    const lat = position.coords.latitude;
-    const long = position.coords.longitude;
-    userPosition = {lat, long};
+  const lat = position.coords.latitude;
+  const long = position.coords.longitude;
+  userPosition = { lat, long };
 };
 navigator.geolocation.getCurrentPosition(success, console.log);
 
-function submitForm(e){
-    e.preventDefault();
-    
-    var name = getInputVal('name');
-    var email = getInputVal('email');
-    var message = getInputVal('message');
-    
+function submitForm(e) {
+  e.preventDefault();
 
-    db.doc().set({
-        name: name,
-        email: email,
-        message: message,
-        location:userPosition
-    }).then(function(){
-        console.log('Data Sent');
-        alert('Data has been successfully sent');
-        document.getElementById('name').value = "";
-        document.getElementById('email').value = "";
-        document.getElementById('message').value = "";
-    })
-    .catch(function(){
-        console.log(error);
-    }); 
-    
+  const name = getInputVal('name');
+  const email = getInputVal('email');
+  const message = getInputVal('message');
+
+  db.doc().set({
+    name,
+    email,
+    message,
+    location: userPosition,
+  }).then(() => {
+    console.log('Data Sent');
+    // alert('Data has been successfully sent');
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('message').value = '';
+  })
+    .catch(() => {
+      console.log(error);
+    });
 }
-
-function getInputVal(id){
-    return document.getElementById(id).value;
-}
-
-
-
-
-
--- UI/Contact-page/queries.js
+++ UI/Contact-page/queries.js
@@ -1,27 +1,27 @@
 const queriesList = document.querySelector('.queriesList');
 
-function addQueries(doc){
-    let li = document.createElement('li');
-    let UserName = document.createElement('h3');
-    let userEmail = document.createElement('a');
-    let queryBody = document.createElement('p');
+function addQueries(doc) {
+  const li = document.createElement('li');
+  const UserName = document.createElement('h3');
+  const userEmail = document.createElement('a');
+  const queryBody = document.createElement('p');
 
-    li.setAttribute('data-id', doc.id);
+  li.setAttribute('data-id', doc.id);
 
-    UserName.textContent = doc.data().name;
-    userEmail.textContent = doc.data().email;
-    queryBody.textContent = doc.data().message;
-    
-    li.appendChild(UserName);
-    li.appendChild(userEmail);
-    li.appendChild(queryBody);
+  UserName.textContent = doc.data().name;
+  userEmail.textContent = doc.data().email;
+  queryBody.textContent = doc.data().message;
 
-    queriesList.appendChild(li);
+  li.appendChild(UserName);
+  li.appendChild(userEmail);
+  li.appendChild(queryBody);
+
+  queriesList.appendChild(li);
 }
 
 db.collection('contactData').get().then((snapshot) => {
-    snapshot.docs.forEach(doc =>{
-        addQueries(doc);
-        console.log('Received');   
-    })
-})
+  snapshot.docs.forEach((doc) => {
+    addQueries(doc);
+    console.log('Received');
+  });
+});
